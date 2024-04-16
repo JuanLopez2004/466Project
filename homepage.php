a8231cda45e834f0c5a8a9d137540e8b3d1d3eea
nate <naguirre0521@gmail.com>
2024-04-16 15:28:09 -0500

    Added skeleton version of homepage html

---
+++ homepage.php
@@ -0,0 +1,23 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>iKaraoke</title>
+
+<h1 style="text-align:center;">iKaraoke</h1>
+</head>
+<body style="text-align:center;">
+  <style>
+    div{
+      width: 300px;
+      margin: auto;
+    }
+  </style>
+  <div> 
+    <h2>Search</h2>
+    <form>
+    <label for="search">Enter a search:</label><br>
+    <input type"text" id="search" name="search"><br>
+    </form>
+  </div>
+</body>
+</html> 
