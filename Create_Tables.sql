1f02df69dae25a34e1c95aaa7bc8fd0fc2cdb882
Juan Lopez <108855394+JuanLopez2004@users.noreply.github.com>
2024-04-17 16:14:53 -0500

    Add files via upload
    source /home/hopper/YOURZID/Create_Tables.sql

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -6,6 +6,13 @@ DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Producer;
 DROP TABLE IF EXISTS Writer;
 
+
+--added--
+DROP TABLE IF EXISTS PriorityQueue;
+DROP TABLE IF EXISTS PQuser;
+DROP TABLE IF EXISTS FIUser;
+DROP TABLE IF EXISTS File;
+DROP TABLE IF EXISTS FIFOInput;
 -- create tables -- 
 
 -----one to many relationships-----
@@ -63,25 +70,80 @@ CREATE TABLE SongProducer
 );
 
 -- kfile -- julian
+CREATE TABLE KaraokeFile
+(
+    FileName INT,
+    SongID INT,
+    FOREIGN KEY (SongID) REFERENCES Song(ID),
+    PRIMARY KEY (FileName, SongID)
+);
 
+--file--
 
--- priority queue input -- julian 
-
-
--- fifo queue --  julian 
-
-
--- 'is stored in' -- julian 
-
+CREATE TABLE File
+(
+    FileName INT AUTO_INCREMENT PRIMARY KEY
+);
 
 -- priority queue -- juan
 
+CREATE TABLE PriorityQueue
+(
+    PQID INT PRIMARY KEY,
+    PTIME TIME,
+    Pledge INT
+);
+
+
+-- priority queue input -- julian 
+CREATE TABLE PriorityQueueInput
+(
+    PQID INT, --Foreign Key
+    FileName INT, --Foreign Key
+    PQUser INT AUTO_INCREMENT,
+    PQFName INT,
+    PQLName INT,
+    FOREIGN KEY (PQID) REFERENCES PriorityQueue(PQID),
+    FOREIGN KEY (File_Name) REFERENCES KaraokeFile(File_Name),
+    PRIMARY KEY (PQID, File_Name, PQUser)
+);
 
 -- priority user -- juan
 
+CREATE TABLE PQUser
+(
+    PQUser INT AUTO_INCREMENT PRIMARY KEY,
+    PQ_Fname VARCHAR(40),
+    PG_Lname VARCHAR(40)
+);
 
 -- fifo user --  juan
 
+CREATE TABLE FIUser
+(
+    FIUser INT AUTO_INCREMENT PRIMARY KEY,
+    FI_FName VARCHAR(40),
+    FI_LNAME VARCHAR(40)
+);
+
+-- fifo queue --  julian 
+CREATE TABLE FirstInQueue
+(
+    FQID INT,
+    Time INT,
+    PRIMARY KEY (FQID)
+);
 
 -- fifo queue input -- juan
 
+CREATE TABLE FIFOInput
+(
+    PID INT AUTO_INCREMENT PRIMARY KEY,
+    FUser INT NOT NULL,
+    FQID INT NOT NULL,
+    FileName INT NOT NULL, 
+    FIFName VARCHAR(40),
+    FILName VARCHAR(40),
+    FOREIGN KEY (FQID) REFERENCES FIFOQueue(ID),
+    FOREIGN KEY (FileName) REFERENCES File(ID)
+);
