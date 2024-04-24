12436f3abcfc7deb6f71de4466f857a920d02870
JulianC1121 <Jcarranza03.jc@gmail.com>
2024-04-24 03:32:39 -0500

    Added the tables and info from the PQUser and PQueue, as well as FIFOUser and FIFOQueue, added a bit of CSS as well.

--- dj.php
+++ dj.php
@@ -15,13 +15,17 @@ try
     // Set PDO to throw exceptions
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Fetch Priority Queue table info
-    $stmt = $pdo->query("SELECT * FROM PriorityQueue");
-    $PriorityQueue = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // Fetch FIUser and FIFOQueue table info
+    $stmt = $pdo->query("SELECT FIUser.*, FIFOQueue.FileID, FIFOQueue.Time 
+                         FROM FIUser 
+                         JOIN FIFOQueue ON FIUser.FIUser = FIFOQueue.FIID");
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Fetching the FIFOQueue table info
-    $stmt = $pdo->query("SELECT * FROM FIFOQueue");
-    $FIFOQueue = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // Fetch PQUser and Priority Queue table info
+    $stmt = $pdo->query("SELECT PQUser.*, PriorityQueue.FileID, PriorityQueue.Time 
+                         FROM PQUser 
+                         JOIN PriorityQueue ON PQUser.PQUser = PriorityQueue.PQID");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
@@ -31,6 +35,32 @@ try
 
 <html>
 <style>
+    color: white;
+        table
+        {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table, th, td
+        {
+            margin-left: auto;
+            margin-right: auto;
+            border: 2px solid white;
+            padding: 5px;
+            text-align: center;
+        }
+
+        th
+        {
+            background-color: #000000;
+            color: white;
+        }
+
+        td
+        {
+            background-color: #FF0000;
+        }
   body{
     text-align: center;
   }
@@ -42,6 +72,53 @@ try
 
 <body>
   <h1>DJ Interface</h1>
+
+  <section>
+  <h2 style="color: black;">Priority Queue</h2>
+        <table>
+            <tr>
+                <th>Queue</th>
+                <th>User First Name</th>
+                <th>User Last Name</th>
+                <th>Pledge</th>
+                <th>KaraokeID</th>
+            </tr>
+            <?php foreach ($results as $results) : ?>
+                <tr>
+                    <td><?php echo $results['PQUser']; ?></td>
+                    <td><?php echo $results['PQ_FName']; ?></td>
+                    <td><?php echo $results['PQ_LName']; ?></td>
+                    <td><?php echo $results['Pledge']; ?></td>
+                    <td><?php echo $results['FileID']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+
+        <h2 style="color: black;">Priority Queue</h2>
+        <table>
+            <tr>
+                <th>Queue</th>
+                <th>User First Name</th>
+                <th>User Last Name</th>
+                <th>KaraokeID</th>
+            </tr>
+            <?php foreach ($result as $result) : ?>
+                <tr>
+                    <td><?php echo $result['FIUser']; ?></td> 
+                    <td><?php echo $result['FI_FName']; ?></td>
+                    <td><?php echo $result['FI_LNAME']; ?></td>
+                    <td><?php echo $result['FileID']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+
+  </section>
+
+  <?php
+    
+  ?>
+
+
 </body>
 
 </html>
