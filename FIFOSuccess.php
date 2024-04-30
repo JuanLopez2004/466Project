5ff2434215cb2208419ce3d982e5e9113129d7f1
Juan Lopez <108855394+JuanLopez2004@users.noreply.github.com>
2024-04-30 12:09:47 -0500

    Add files via upload

---
+++ FIFOSuccess.php
@@ -0,0 +1,73 @@
+<?php
+require 'pdo.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<style>
+body {
+    text-align: center;
+}
+</style>
+<meta http-equiv="refresh" content="5;url=homepage.php">
+
+<?php
+    try 
+    {
+        $dsn = "mysql:host=courses;dbname=z1952526";
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          $sql = "SELECT FI_Fname, FI_Lname, FileName FROM FIUser JOIN FIFOQueue ON FIUser.FIUser = FIFOQueue.FIID 
+          JOIN KaraokeFile ON FIFOQueue.FileID = KaraokeFile.FileID WHERE FIUser.FIUser = 1";
+
+          $stmt = $pdo->query($sql);
+          if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+              $first_name = $row['FI_FName'];
+              $last_name = $row['FI_LName'];
+              $song_name = $row['FileName']; 
+              echo "<h1>Thank You for Choosing John's Basement! </h1<";
+              echo "<h1> Your song has been added to the queue.</h1>";
+              echo "<h1> You will now be redirected to the homepage.</h1>";
+          } 
+          else 
+          {
+            echo "<h1>No results found</h1>";
+          }
+    }
+    catch(PDOexception $e) 
+    { 
+        echo "Connection to database failed: " . $e->getMessage();
+    }
+?>
+ <img src="https://i.imgur.com/qe4Qnc9.jpeg" width="600">
+ <h1 id="countdown">Redirecting in 5 seconds...</h1>
+ </html>
+
+<script>
+    var seconds = 5; 
+    var countdown = document.getElementById('countdown');
+
+    function updateCountdown() 
+    {
+        countdown.innerHTML = 'Redirecting in ' + seconds + ' seconds...';
+        if (seconds <= 0) 
+        {
+            window.location.href = 'homepage.php';
+        }
+        else 
+        {
+            seconds--;
+            setTimeout(updateCountdown, 1000);
+        }
+    }
+
+    updateCountdown();
+</script>
+
+</body>
+<footer>
+  <h3><a href="homepage.php">Home</a></h3>
+</footer>
+</html>
